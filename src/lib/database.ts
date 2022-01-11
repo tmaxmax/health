4882c2486e29fd02d40dafb5669155c48509e954
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-01-11 15:00:36 +0200

    Create DB querying utilities

---
+++ src/lib/database.ts
@@ -0,0 +1,30 @@
+import { Pool, DatabaseError } from 'pg'
+import type { QueryConfig, ClientBase, QueryResult } from 'pg'
+
+const pool = new Pool({ ssl: import.meta.env.PROD })
+
+export function query<R, I extends unknown[]>(config: QueryConfig<I>): Promise<QueryResult<R>> {
+	return pool.query(config)
+}
+
+export async function transact(run: (conn: ClientBase) => Promise<void>): Promise<void> {
+	const conn = await pool.connect()
+
+	try {
+		await conn.query('BEGIN')
+		await run(conn)
+		await conn.query('COMMIT')
+	} catch (err) {
+		try {
+			await conn.query('ROLLBACK')
+		} catch (rollbackErr) {
+			void rollbackErr
+		}
+
+		throw err
+	} finally {
+		conn.release()
+	}
+}
+
+export { DatabaseError }
