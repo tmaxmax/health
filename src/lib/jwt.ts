0ebc1d2ff12124769158865d6768961edd8c6079
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-01-13 09:11:55 +0200

    Crete custom JWT verification error
    This allows us to handle these errors specifically, using instanceof

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,12 +1,32 @@
 import { SignJWT, importJWK, jwtVerify, JWTPayload } from 'jose'
 import type { JWTVerifyResult, JWTVerifyOptions } from 'jose'
 
+if (!process.env.JWK) {
+	throw new Error('JWK environment variable not provided')
+}
+
 const jwk = JSON.parse(process.env.JWK)
 export const key = await importJWK(jwk)
 export const ISSUER = 'https://health.com'
 
-export function verify(jwt: string | Uint8Array, opts: Omit<JWTVerifyOptions, 'issuer'>): Promise<JWTVerifyResult> {
-	return jwtVerify(jwt, key, { issuer: ISSUER, ...opts })
+export class JWTVerifyError extends Error {
+	constructor(public cause: Error) {
+		super(cause.message)
+		this.name = cause.name
+		this.stack = cause.stack
+	}
+}
+
+export async function verify(jwt: string | Uint8Array, opts: Omit<JWTVerifyOptions, 'issuer'>): Promise<JWTVerifyResult> {
+	try {
+		return await jwtVerify(jwt, key, { issuer: ISSUER, ...opts })
+	} catch (err) {
+		if (err instanceof Error) {
+			throw new JWTVerifyError(err)
+		}
+
+		throw err
+	}
 }
 
 class JWT extends SignJWT {
