d415cff28d80335714c1482d0736e931eac0452e
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2022-01-13 09:29:47 +0200

    Fix type errors and check env vars validity

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,17 +1,33 @@
 import { createTransport } from 'nodemailer'
+import type SMTPPool from 'nodemailer/lib/smtp-pool'
 
-const transporter = createTransport({
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore This works according to docs, wrong types?
+if (!process.env.SMTP_HOST) {
+	throw new Error('SMTP host not provided')
+} else if (!process.env.SMTP_PORT) {
+	throw new Error('SMTP host not provided')
+} else if (!process.env.SMTP_USER) {
+	throw new Error('SMTP user not provided')
+} else if (!process.env.SMTP_PASS) {
+	throw new Error('SMTP pass not provided')
+}
+
+const port = parseInt(process.env.SMTP_PORT)
+if (!port) {
+	throw new Error('SMTP port is invalid')
+}
+
+const options: SMTPPool.Options = {
 	host: process.env.SMTP_HOST,
-	port: process.env.SMTP_PORT,
+	port,
 	secure: import.meta.env.PROD,
 	auth: {
 		user: process.env.SMTP_USER,
 		pass: process.env.SMTP_PASS,
 	},
 	pool: true,
-})
+}
+
+const transporter = createTransport(options)
 
 await transporter.verify()
 
